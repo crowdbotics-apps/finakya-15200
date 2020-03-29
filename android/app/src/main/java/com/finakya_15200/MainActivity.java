658972d9b2f8c11049600af351958bcc3d39d533
Crowdbotics <team@crowdbotics.com>
2020-03-29 22:12:10 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/finakya_15200/MainActivity.java
@@ -0,0 +1,15 @@
+package com.finakya_15200;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "finakya_15200";
+    }
+}
